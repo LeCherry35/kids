6bede6931f4e1912f67ba54f84d193959772a234
Cherry <lenyamm35@gmail.com>
2024-06-24 13:33:16 +0300

    додав коментарі

--- array/data.js
+++ array/data.js
@@ -1,7 +1,9 @@
-let divId = document.querySelector('#div1')
+// обираєио всі елементи з класом div4ik
 let divClass = document.querySelectorAll('.div4ik')
+// обираємо елемент input
 let input = document.querySelector('input')
 
+// функція яка відповідає за зміну кольору блоків
 let func = () => {
     let num = input.value
     let selectedDiv = divClass[num]
@@ -13,6 +15,8 @@ let func = () => {
     divClass[5].style.backgroundColor = 'white'
     selectedDiv.style.backgroundColor = 'red'
 }
+
+// ставимо функцію на зміну значення input
 input.onchange = func
 
 
