ce85c887d418c9fa3cabd7b5879deb86c876f3f7
Cherry <lenyamm35@gmail.com>
2024-06-24 13:37:59 +0300

    додав приклади массивів і об'єктів

--- array/data.js
+++ array/data.js
@@ -1,3 +1,20 @@
+// массиви
+let arr = [1, 2, 3, 4, 5]
+let arr2 = ['a', 'b', 'c', 'd', 'e']
+let arr3 = [1, 'a', 2, 'b', 3, 'c', 4, 'd', 5, 'e']
+
+// об'єкти
+let obj = {
+    name: 'Vasya',
+    age: 25,
+    isMarried: false
+}
+let obj2 = {
+    name: 'Petya',
+    age: 30,
+    isMarried: true
+}
+
 // обираєио всі елементи з класом div4ik
 let divClass = document.querySelectorAll('.div4ik')
 // обираємо елемент input
